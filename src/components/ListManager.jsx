e615fc2571f8f38d471abdd787d40e14086c90bd
Aayush Rijal <reizal.ayz@gmail.com>
2016-07-04 11:58:12 +0545

    initial commit

---
+++ src/components/ListManager.jsx
@@ -0,0 +1,45 @@
+var React = require('react');
+var List = require('./List.jsx');
+
+var ListManager = React.createClass({
+    getInitialState: function(){
+        return {items: [], newItemText:''};
+    },
+
+    onChange: function(e){
+        this.setState({newItemText: e.target.value});
+    },
+
+    handleSubmit: function(e){
+        e.preventDefault();
+
+        var currentItems = this.state.items;
+
+        currentItems.push(this.state.newItemText);
+
+        this.setState({items: currentItems, newItemText:''});
+    },
+
+    render: function(){
+        return (
+                <div className="col-sm-4">
+                    {/*<div className="panel panel-default">*/}
+                        {/*<div className="panel-heading">*/}
+                            <h3>{this.props.title}</h3>
+                        {/*</div>*/}
+                        <div className="panel-body">
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="input-group">
+                                    <input type="text" className="form-control" onChange={this.onChange} value={this.state.newItemText} />
+                                </div>
+                                <button type="button" className="btn btn-primary btn-sm">Add</button>
+                            </form>
+                            <List items = {this.state.items} />
+                        </div>
+                    {/*</div>*/}
+                </div>
+        );
+    }
+});
+
+module.exports = ListManager;
